e9d9aafb0ee57fae67d9fbead0f76479907725cc
Matt Hong <matthew.m.hong@gmail.com>
2020-10-07 17:11:30 -0700

    changed to not use middleware, started building api for specific product ids

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 const express = require('express');
 const path = require('path');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const axios = require('axios');
 
 const app = express();
 const PORT = 8000;
 
-const proxyProductOptions = createProxyMiddleware({target: 'http://localhost:3000', changeOrigin: true}) // 3000
-const proxyBottomCarousel = createProxyMiddleware({target: 'http://localhost:3001', changeOrigin: true}) // 3001
-const proxyProductView = createProxyMiddleware({target: 'http://localhost:3002', changeOrigin: true}) // 3002
-const proxyReview = createProxyMiddleware({target: 'http://localhost:3003', changeOrigin: true}) // 3003
-
 app.use(express.static(path.join(__dirname, 'public')));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+app.use('/products', express.static(path.join(__dirname, 'client', 'dist')));
 
-app.use('/', proxyProductOptions)
-app.use('/', proxyBottomCarousel)
-app.use('/', proxyProductView)
-app.use('/', proxyReview)
-app.use('/:productId', proxyReview)
+app.get('/products/*', (req, res) => {
+  console.log('testing')
+  // axios.get('http://localhost:3000/products/*');
+  axios.get('http://localhost:3003/products/*', (req, res) => {
+    console.log('inside here')
+  }).catch((error) => res.send(error));
+});
+// make individual server api calls
 
 app.listen(PORT, () => {
   console.log('listening on port: ' + PORT);
